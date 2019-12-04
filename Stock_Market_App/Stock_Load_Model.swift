6b74739cb8cb80304071f12cb9a0e9b0ab5f74af
Devjeel <jeelhp02@gmail.com>
2019-12-04 11:01:26 -0500

    Created Load View

---
+++ Stock_Market_App/Stock_Load_Model.swift
@@ -0,0 +1,13 @@
+//
+//  Stock_Load_Model.swift
+//  Stock_Market_App
+//
+//  Created by Jeel Patel on 2019-12-04.
+//  Copyright © 2019 Jeel Patel. All rights reserved.
+//
+
+import Foundation
+
+class Stock_Market_App {
+    
+}
