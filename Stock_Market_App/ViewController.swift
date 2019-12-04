dbb209b312dc3b580d582e3d6a0a539aff6c7a2f
Devjeel <jeelhp02@gmail.com>
2019-12-04 10:40:40 -0500

    Created Welcome Screen

---
+++ Stock_Market_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stock_Market_App
+//
+//  Created by Jeel Patel on 2019-12-04.
+//  Copyright © 2019 Jeel Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
